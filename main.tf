61fbb05e88612c686b2b761052cc36ddda6a111c
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-04-04 23:05:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "spacelift_module" "k8s-module2" {
   name               = "k8s-module2"
-  terraform_provider = "aws"
+  terraform_provider = "default"
   administrative     = true
   branch             = "main"
   repository         = "tftest"
